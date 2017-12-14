9dfb85148dd4aec74507743ac98762a994f7676b
tobyaries <yestin.tian@qq.com>
2017-12-14 20:00:16 +0800

    use latest MoonUI, and import all of it

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { CountDown, InfiniteScroll } from 'moon-ui'
+// import { CountDown, InfiniteScroll } from 'moon-ui'
+import MoonUI from 'moon-ui'
 import axios from 'axios'
 
 Vue.prototype.$ajax = axios
 Vue.config.productionTip = false
-Vue.use(CountDown)
-Vue.use(InfiniteScroll)
+// Vue.use(CountDown)
+// Vue.use(InfiniteScroll)
+Vue.use(MoonUI)
 
 new Vue({
   el: '#app',
--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="catalog">
     <h1>{{ msg }}</h1>
-    <h2>UI Links</h2>
+    <h2>Basic Components</h2>
     <ul>
-      <li><router-link to="/countdown">Go to countdown</router-link></li>
-      <li><router-link to="/infinitescroll">Go to infinite-scroll</router-link></li>
+      <li><router-link to="/countdown">Go to moon-count-down</router-link></li>
+      <li><router-link to="/infinitescroll">Go to moon-infinite-scroll</router-link></li>
     </ul>
   </div>
 </template>
@@ -39,11 +39,18 @@ h1 {
 ul {
   list-style-type: none;
   padding: 0;
+  overflow: hidden;
+  border-bottom: 1px solid #ddd;
 }
 li {
   margin: 0 10px;
+  text-align: center;
+  overflow: hidden;
+  padding: 10px;
+  border-top: 1px solid #ddd;
 }
 a {
   color: #42b983;
+  text-decoration: none;
 }
 </style>
